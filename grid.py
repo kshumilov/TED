493b63d52ce0f09b70e092fea717807dd690235a
Kirill Shumilov <shumilov.kirill98@gmail.com>
2018-03-16 10:37:40 -0700

    Finished calculation

--- grid.py
+++ grid.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python3
+
 import pickle
+from tempfile import mkdtemp
+from shutil import rmtree
 
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import Normalizer
@@ -10,19 +14,25 @@ from sklearn.metrics import silhouette_score
 
 def silhouette_metric(estimator, X):
     clusters = estimator.fit_predict(X)
-    score = silhouette_score(estimator.steps[0][1].transform(X), clusters)
+    score = silhouette_score(
+        estimator.named_steps['lsa'].transform(X),
+        clusters
+    )
     return score
 
 
 with open('Tf-Idf_data.pickle', 'rb') as model_pickle:
     data = pickle.load(model_pickle)
 
+cachedir = mkdtemp()
 pipe = Pipeline(steps=[
-    ('lsa', Pipeline(steps=[
-        ('svd', TruncatedSVD()),
-        ('norm', Normalizer(copy=False))
-    ])),
-    ('kmeans', KMeans(precompute_distances=True, n_jobs=-1))
+    ('lsa', Pipeline(
+        memory=cachedir,
+        steps=[
+            ('svd', TruncatedSVD()),
+            ('norm', Normalizer(copy=False))
+        ])),
+    ('kmeans', KMeans(precompute_distances=True))
 ])
 
 parms = {
@@ -30,8 +40,16 @@ parms = {
     'lsa__svd__n_components': range(50, 650, 50)
 }
 
-grid = GridSearchCV(pipe, param_grid=parms, scoring=silhouette_metric, verbose=2, n_jobs=-1)
+grid = GridSearchCV(
+    pipe,
+    param_grid=parms,
+    scoring=silhouette_metric,
+    verbose=3,
+    n_jobs=-1
+)
 grid.fit(data)
 
 with open('grid.pickle', 'wb') as f:
     pickle.dump(grid, f)
+
+rmtree(cachedir)
